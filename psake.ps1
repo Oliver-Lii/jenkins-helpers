56b5d9cf0ec53020ff0bc4c5d2d989be751a04d5
Oliver Li <oli@sdl.com>
2017-07-18 14:58:40 +0100

    AppVeyor Configuration

---
+++ psake.ps1
@@ -0,0 +1,65 @@
+# PSake makes variables declared here available in other scriptblocks
+# Init some things
+Properties {
+    # Find the build folder based on build system
+    $ProjectRoot = $ENV:BHProjectPath
+    if(-not $ProjectRoot)
+    {
+        $ProjectRoot = $PSScriptRoot
+    }
+
+    $Verbose = @{}
+    if($ENV:BHCommitMessage -match "!verbose")
+    {
+        $Verbose = @{Verbose = $True}
+    }
+}
+
+Task Default -Depends Deploy
+
+Task Init {
+    '----------------------------------------------------------------------'
+    Set-Location $ProjectRoot
+    "Build System Details:"
+    Get-Item ENV:BH*
+    "`n"
+}
+
+Task Test -Depends Init  {
+
+}
+
+Task Build -Depends Test {
+    '----------------------------------------------------------------------'
+    #Set-ModuleFunctions
+}
+
+Task Deploy -Depends Build {
+    '----------------------------------------------------------------------'    
+    # Update Manifest version number
+    $ManifestPath = $Env:BHPSModuleManifest
+    
+    If (-Not $env:APPVEYOR_BUILD_VERSION) {
+        $Manifest = Test-ModuleManifest -Path $manifestPath
+        [System.Version]$Version = $Manifest.Version
+        [String]$NewVersion = New-Object -TypeName System.Version -ArgumentList ($Version.Major, $Version.Minor, $Version.Build, ($Version.Revision+1))
+    } Else {
+        $NewVersion = $env:APPVEYOR_BUILD_VERSION
+    }
+    "New Version: $NewVersion"
+
+    $FunctionList = @((Get-Module $ManifestPath -ListAvailable).ExportedCommands.Values.Name)
+
+    Update-ModuleManifest -Path $ManifestPath -ModuleVersion $NewVersion -FunctionsToExport $functionList
+    (Get-Content -Path $ManifestPath) -replace "PSGet_$Env:BHProjectName", "$Env:BHProjectName" | Set-Content -Path $ManifestPath
+    (Get-Content -Path $ManifestPath) -replace 'NewManifest', "$Env:BHProjectName" | Set-Content -Path $ManifestPath
+    (Get-Content -Path $ManifestPath) -replace 'FunctionsToExport = ', 'FunctionsToExport = @(' | Set-Content -Path $ManifestPath -Force
+    (Get-Content -Path $ManifestPath) -replace "$($FunctionList[-1])'", "$($FunctionList[-1])')" | Set-Content -Path $ManifestPath -Force
+
+    $Params = @{
+        Path = $ProjectRoot
+        Force = $true
+        Recurse = $false # We keep psdeploy artifacts, avoid deploying those : )
+    }
+    Invoke-PSDeploy @Verbose @Params
+}
