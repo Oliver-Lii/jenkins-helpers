7ac3e88883c81fa8e9d0ff370621ee4fe07f4b66
Sam Martin <smartin@sdl.com>
2017-08-16 11:31:33 +0100

    Monkeypatching option to allow complete replacement of write-* cmdlets

---
+++ Jenkins-Helpers/Public/Set-JenkinsAnsiAliases.ps1
@@ -0,0 +1,15 @@
+﻿<#
+.Synopsis
+   This cmdlet monkeypatches Write-Verbose, Warning, Error, Debug, and Info to prepend them with the appropriate ANSI colors for use in Jenkins
+.DESCRIPTION
+   This cmdlet monkeypatches Write-Verbose, Warning, Error, Debug, and Info to prepend them with the appropriate ANSI colors for use in Jenkins
+.EXAMPLE
+   Set-JenkinsAnsiAliases
+#>
+Function Set-JenkinsAnsiAliases{
+    $Aliases = @("Write-Verbose", "Write-Info", "Write-Warning", "Write-Error", "Write-Debug")
+    foreach($Alias in $Aliases){
+        Write-Verbose "Aliasing $Alias to Write-JenkinsAnsi"
+        Set-Alias $Alias Write-JenkinsAnsi -Scope global
+    }
+}
